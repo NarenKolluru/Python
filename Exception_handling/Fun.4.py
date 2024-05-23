081352c7aac2eca15853db959e74a22ae9db7b2e
NarenKolluru <naren.k@thundersoft.com>
2024-05-23 11:49:24 +0530

    Add files via upload

---
+++ Exception_handling/Fun.4.py
@@ -0,0 +1,13 @@
+try:
+   num = int(input("Enter a number: "))
+   factorial = 1
+   if num < 0:       
+      raise ValueError
+   elif num == 0:
+        print("The factorial of 0 is 1")
+   else:
+       for i in range(1,num + 1):
+          factorial = factorial*i
+       print("The factorial of",num,"is",factorial)
+except ValueError: 
+       print("Sorry, factorial does not exist for negative numbers")    
