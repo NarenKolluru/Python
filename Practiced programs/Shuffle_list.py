ec0049697255fbefa83710962342f6642f9bb657
NarenKolluru <naren.k@thundersoft.com>
2024-05-23 13:02:32 +0530

    Add files via upload

---
+++ Practiced programs/Shuffle_list.py
@@ -0,0 +1,14 @@
+import random
+nums = [3,7,2,5,9]
+print("Original list:")
+print(nums)
+random.shuffle(nums)
+print("Shuffle list:")
+print(nums)
+words = ['red', 'black', 'green', 'blue']
+print("\nOriginal list:")
+print(words)
+random.shuffle(words)
+print("Shuffle list:")
+print(words)
+
